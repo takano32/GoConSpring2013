79f43cddc96f7dc1cc51a747a83f5418ce434404
TAKANO Mitsuhiro <takano32@gmail.com>
2013-04-13 13:29:14 +0900

    parallelCalc: loop num of procs times.

--- BBP/bbp.go
+++ BBP/bbp.go
@@ -5,9 +5,9 @@ import (
 	"math"
 )
 
-func loopCalc() float64 {
+func loopCalc(n int) float64 {
 	pi := float64(0)
-	for i := 0; i < 10; i++ {
+	for i := 0; i < n; i++ {
 		j := float64(i)
 		var x1 float64 = 4 / (8 * j + 1)
 		var x2 float64 = 2 / (8 * j + 4)
@@ -18,30 +18,34 @@ func loopCalc() float64 {
 	return pi
 }
 
-func parallelCalc() float64 {
+func parallelCalc(n int) float64 {
 	pi := float64(0)
-	k := func(i int, c chan float64) {
-		j := float64(i)
-		var x1 float64 = 4 / (8 * j + 1)
-		var x2 float64 = 2 / (8 * j + 4)
-		var x3 float64 = 1 / (8 * j + 5)
-		var x4 float64 = 1 / (8 * j + 6)
-		c <- (x1 - x2 - x3 -x4) / math.Pow(16, j)
+	k := func(i int,procs int, c chan float64) {
+		var sum float64
+		for ii := procs * i; ii < i + procs; ii++ {
+			j := float64(ii)
+			var x1 float64 = 4 / (8 * j + 1)
+			var x2 float64 = 2 / (8 * j + 4)
+			var x3 float64 = 1 / (8 * j + 5)
+			var x4 float64 = 1 / (8 * j + 6)
+			sum += (x1 - x2 - x3 -x4) / math.Pow(16, j)
+		}
+		c <- sum
 	}
-	c := make(chan float64)
-	n := int(10)
-	for i := 0; i < n; i++ {
-		go k(i, c)
+	procs := 100
+	c := make(chan float64, 16)
+	m := n / procs
+	for i := 0; i < m; i++ {
+		go k(i, procs, c)
 	}
-	for i := 0; i < n; i++ {
+	for i := 0; i < m; i++ {
 		pi += <- c
 	}
 	return pi
 }
 
 func main() {
-	fmt.Println(loopCalc())
-	fmt.Println(parallelCalc())
+	fmt.Println(loopCalc(1000))
+	fmt.Println(parallelCalc(1000))
 }
-
 
