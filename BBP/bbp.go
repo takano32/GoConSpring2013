a62499983f80235a03e5f13287ced9b0c52bf63a
TAKANO Mitsuhiro <takano32@gmail.com>
2013-04-13 11:20:12 +0900

    parallelCalc func.

--- BBP/bbp.go
+++ BBP/bbp.go
@@ -18,9 +18,30 @@ func loopCalc() float64 {
 	return pi
 }
 
+func parallelCalc() float64 {
+	pi := float64(0)
+	k := func(i int, c chan float64) {
+		j := float64(i)
+		var x1 float64 = 4 / (8 * j + 1)
+		var x2 float64 = 2 / (8 * j + 4)
+		var x3 float64 = 1 / (8 * j + 5)
+		var x4 float64 = 1 / (8 * j + 6)
+		c <- (x1 - x2 - x3 -x4) / math.Pow(16, j)
+	}
+	c := make(chan float64)
+	n := int(10)
+	for i := 0; i < n; i++ {
+		go k(i, c)
+	}
+	for i := 0; i < n; i++ {
+		pi += <- c
+	}
+	return pi
+}
 
 func main() {
 	fmt.Println(loopCalc())
+	fmt.Println(parallelCalc())
 }
 
 
